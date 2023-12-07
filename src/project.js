49e50de4eca84e4e6003ad976b3c9d33ff7a1c5a
Tilen Marš <its0black0magic@gmail.com>
2023-12-07 14:33:07 +0000

    Write basic functions for to do items and projects

--- src/project.js
+++ src/project.js
@@ -1,6 +1,11 @@
 function project(title, description) {
     this.title = title;
     this.description = description;
+    this.toDoItems = new Array();
+
+    function addToDoItem(item) {
+        toDoItems.push(item);
+    }
     return {
         title, 
         description,
--- src/todo-item.js
+++ src/todo-item.js
@@ -1,7 +1,8 @@
-function todoItem(title, description, dueDate, priority) {
+function todoItem(title, description='', dueDate = 'false', priority='low', dueTime = 'false') {
     this.title = title;
     this.description = description;
     this.dueDate = dueDate;
+    this.dueTime = dueTime;
     this.priority = priority;
     return {
         title, 
