1d97b86d93a4eee6349cb7af234e708f48862358
Tilen Marš <its0black0magic@gmail.com>
2023-12-08 17:18:50 +0000

    Added "done" property to todoItem.

--- src/todo-item.js
+++ src/todo-item.js
@@ -1,12 +1,15 @@
-export function todoItem(title, description='', dueDate = 'false', priority='low', dueTime = 'false') {
+export function todoItem(title, description='', dueDate = 'false', dueTime = 'false', priority='low') {
     this.title = title;
     this.description = description;
     this.dueDate = dueDate;
     this.dueTime = dueTime;
     this.priority = priority;
+    this.done = false;
     return {
         title, 
         description,
-        dueDate
+        dueDate,
+        dueTime,
+        priority
     }
 }
